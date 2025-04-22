cc09aeafb982c87fbb6d560e7b8075be1f4db15f
Ignacio Torrero <ignacio_torrero97@hotmail.com>
2025-04-22 14:08:43 -0300

    microservicios 04
    Se convierte al rating-service en el tercer microservicio de la aplicacion.

---
+++ movie-match-api/rating-service/src/services/rating.service.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from "@prisma/client";
+
+export const prisma = new PrismaClient();
+
+export const rateMovie = async (userId: number, movieId: number, score: number) => {
+  if (score < 1 || score > 5) {
+    throw new Error("La calificación debe estar entre 1 y 5 estrellas.");
+  }
+
+  const movie = await prisma.movie.findUnique({ where: { id: movieId } });
+  if (!movie) {
+    throw new Error("La película no existe.");
+  }
+
+  const existingRating = await prisma.rating.findFirst({
+    where: { userId, movieId },
+  });
+
+  if (existingRating) {
+    try {
+      return await prisma.rating.update({
+        where: { id: existingRating.id },
+        data: { score }
+      });
+    } catch (error) {
+      throw new Error("Error al actualizar la calificación.");
+    }
+  }
+  
+  try {
+    return await prisma.rating.create({
+      data: { userId, movieId, score },
+    });
+  } catch (error) {
+    throw new Error("Error al crear la calificación.");
+  }
+};
