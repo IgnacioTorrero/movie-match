41f3ac5147ff4856c82ed2ae9366c279559da1b2
Ignacio Torrero <ignacio_torrero97@hotmail.com>
2025-04-28 20:33:21 -0300

    correccion-swagger 01
    Se actualiza el swagger acorde a la nueva arquitectura de microservicios.

--- movie-match-api/movie-service/src/app.ts
+++ movie-match-api/movie-service/src/app.ts
@@ -7,10 +7,13 @@ import dotenv from "dotenv";
 import path from "path";
 import movieRoutes from "./routes/movie.routes";
 import { errorHandler } from "./middlewares/error.middleware";
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 const app = express();
+const swaggerDocument = YAML.load(path.resolve(__dirname, "../swagger.yaml"));
 
 app.use(express.json());
 app.use(cors());
@@ -18,5 +21,6 @@ app.use(helmet());
 app.use(morgan("dev"));
 app.use("/api", movieRoutes);
 app.use(errorHandler);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default app;
