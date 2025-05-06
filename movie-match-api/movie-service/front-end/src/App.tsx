514dcf367a6452c45e3be500c8ddabcfc66b7d24
Ignacio Torrero <ignacio_torrero97@hotmail.com>
2025-05-06 17:26:35 -0300

    front-end 09
    Se corrigen imports y dependencias para el movie-service.

--- movie-match-api/movie-service/front-end/src/App.tsx
+++ movie-match-api/movie-service/front-end/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { JSX } from "react";
 import Home from "./pages/Home";
 import CreateMovie from "./pages/CreateMovie";
 import EditMovie from "./pages/EditMovie";
