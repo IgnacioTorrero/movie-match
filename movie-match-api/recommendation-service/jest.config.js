051e4db94b4eef2dadb2367f045668754f3a7a3f
Ignacio Torrero <ignacio_torrero97@hotmail.com>
2025-05-29 18:18:44 -0300

    testing-actualizacion 09
    Se agrega test al recommendation.service.ts.

---
+++ movie-match-api/recommendation-service/jest.config.js
@@ -0,0 +1,15 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  testMatch: [
+    "**/test/**/*.test.ts",
+    "**/?(*.)+(spec|test).[jt]s?(x)",
+  ],
+};
