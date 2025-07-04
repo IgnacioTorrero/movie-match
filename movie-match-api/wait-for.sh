64c8a7d93be251eaf1b87caa000e9eb30e7254da
Ignacio Torrero <ignacio_torrero97@hotmail.com>
2025-07-04 09:53:07 -0300

    optimizacion-imagenes 09
    Indentación de los Dockerfile y corrección del script wait-for.sh.

--- movie-match-api/wait-for.sh
+++ movie-match-api/wait-for.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
 echo "⏳ Esperando a MySQL en mysql:3306..."
-until nc -z -v -w30 mysql 3306
+i=0
+while ! nc -z -v -w30 mysql 3306 2>/dev/null
 do
-  echo "⏳ Esperando a MySQL..."
+  i=$((i+3))
+  printf "\r⏳ %ds esperando a MySQL..." "$i"
   sleep 3
 done
-
+echo ""
 echo "✅ MySQL está listo."
 
 echo "⏳ Esperando a Redis en redis:6379..."
-until nc -z -v -w30 redis 6379
+i=0
+while ! nc -z -v -w30 redis 6379 2>/dev/null
 do
-  echo "⏳ Esperando a Redis..."
+  i=$((i+3))
+  printf "\r⏳ %ds esperando a Redis..." "$i"
   sleep 3
 done
-
+echo ""
 echo "✅ Redis está listo."
 
 echo "🚀 Arrancando app: $@"
